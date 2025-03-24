d8471285852857891fe52d611852ec8d13295baa
keithmosleyjr <keithmosleyjr@gmail.com>
2025-03-24 00:16:33 +0000

    Upgraded and refactored the server.js

--- server.js
+++ server.js
@@ -1,32 +1,41 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
+const db = require("./app/models");
 
+// Create an instance of the Express application
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8081"
+// Configure CORS options
+const corsOptions = {
+  origin: "http://localhost:8081",
 };
 
+// Apply middleware
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+// Sync databse
+db.sequelize
+  .sync();
+  .then(() => {
+    console.log("Database synchronized successfully.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
 
-const db = require("./app/models");
-db.sequelize.sync();
-
-// simple route
-app.get("/", (req, res) => {
+// Define a simple route
+app.get("/", (req, res) =>) {
   res.json({ message: "Welcome to certificate tracker application." });
 });
 
+// Import and use routes
 require("./app/routes/cert.routes")(app);
 
-// set port, listen for requests
+// Set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
